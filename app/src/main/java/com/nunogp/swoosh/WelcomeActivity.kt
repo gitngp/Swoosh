d866f3790340b4b80ec531081de0c8cc7f055832
gitngp <nunogouveiap@gmail.com>
2020-06-20 16:42:24 +0100

    intent_context1>2screen

--- app/src/main/java/com/nunogp/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/nunogp/swoosh/WelcomeActivity.kt
@@ -1,11 +1,19 @@
 package com.nunogp.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        // clicar det started passar 2º screen
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
